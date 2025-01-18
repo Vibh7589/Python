3b8b0b983d7f460087c94058cf0eccd052838ff1
vibhuti dhimar <vibhutidhimar@live.com>
2025-01-18 14:05:10 +0000

    Python

---
+++ test.py
@@ -0,0 +1,8 @@
+number = 5
+
+if number %2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
+    
+
--- testing.py
+++ testing.py
@@ -1,2 +1,28 @@
-print("hellow there!")
-print ("GitHub")
+firstname = "vibhuti"
+lastname = "Dhimar"
+print(firstname.upper() +" "+ lastname.lower())
+
+print(firstname[::-1])
+
+num1 = 5
+num2 = 7
+
+print(not(num1 <= num2))
+
+email = "abc@gmail.com"
+password = "1234567"
+
+if email == "abc@gmail.com" and password == "1234567"
+    print("Email is correct")
+elif password =="1234567":
+    print("password is correct")
+else:
+    print("sorry your password and or email are not correct")
+    
+
+number = int(input("Enter number"))
+if number % 2 == 0 then
+    print("number is even")
+else:
+    print("number is odd")
+
